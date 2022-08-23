565c2e7bfb98774a81751bc9fc3ff513847c6a0d
mikhail <mikhail.vakulin@gmail.com>
2022-08-23 22:51:50 +0300

    напсиал код для сортировки выбором

---
+++ selection_sort/Program.cs
@@ -0,0 +1,24 @@
+﻿/// метод сортировки выбором
+
+
+int[] selectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = i; j < array.Length; j++)
+        {
+            if (array[i] > array[j])
+            {
+
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+    }
+    return array;
+}
+
+int[]array= {10,16,3,1,4,9,78,99,65};
+System.Console.WriteLine(string.Join(", ", selectionSort(array)));
