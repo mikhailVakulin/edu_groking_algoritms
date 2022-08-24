038b4afc20fc261725cddb378959254bf716a2e5
mikhail <mikhail.vakulin@gmail.com>
2022-08-24 16:05:17 +0300

    1

--- quickSort/Program.cs
+++ quickSort/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void quickSort(int[] array)
+{
+    int[]newLeftArr=new int[];
+    int pivot = new Random().Next(array.Length);
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < array[pivot])
+        {
+            for (int j = 0; ; )
+            {
+                newLeftArr[j]= array[i];
+            }
+            
+            
+        }
+    }
+}
+
