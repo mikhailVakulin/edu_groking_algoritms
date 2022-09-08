119af7aeff2efa05b3ce569f8d448b8b24437260
mikhail <mikhail.vakulin@gmail.com>
2022-09-08 09:04:08 +0300

    1

---
+++ leetcode/romanToInteger/Program.cs
@@ -0,0 +1,23 @@
+﻿string s = "MDCL";
+char[] array = s.ToCharArray();
+int result = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == "I") result += 1;
+    else if (array[i] == "V") result += 5;
+    else if (array[i] == "X") result += 10;
+    else if (array[i] == "L") result += 50;
+    else if (array[i] == "C") result += 100;
+    else if (array[i] == "D") result += 500;
+    else if (array[i] == "M") result += 1000;
+}
+return result;
+
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
--- quickSort/Program.cs
+++ quickSort/Program.cs
@@ -1,18 +1,64 @@
-﻿void quickSort(int[] array)
+﻿
+/// метод разбиения массива для быстрой сортировки
+int partition(int[] array, int left, int right) // (массив, первый элемент, последний элемент)
 {
-    int[]newLeftArr=new int[];
-    int pivot = new Random().Next(array.Length);
-    for (int i = 0; i < array.Length; i++)
+    int pivot = array[(left+right)/2];  // индекс опорного элемента
+    int i = left;
+    int j = right;
+
+    while (i <= j)
     {
-        if (array[i] < array[pivot])
+        while (array[i] < pivot) i++;
+        while (array[j] > pivot) j--;
+        if (i <= j)
         {
-            for (int j = 0; ; )
-            {
-                newLeftArr[j]= array[i];
-            }
-            
-            
+            swapElenets(array, i, j);
+            i++;
+            j--;
         }
     }
+    return pivot;
 }
 
+
+
+
+/// метод меняет элементы местами
+void swapElenets(int[] array, int i, int j)  // i,j = индексы элементов для замены
+{
+    int temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+
+}
+
+
+/// метод рекурсивной быстрой сортировки
+int[] quickSort(int[] array, int left, int right)
+{
+    int index; // переменная
+    if (array.Length <= 1) return array;
+    else if (array.Length > 1)
+    {
+        index = partition(array, left, right);
+        if (left < index - 1)
+        {
+            quickSort(array, left, index - 1);
+        }
+        if (index < right)
+        {
+            quickSort(array, index, right);
+        }
+
+        return array;
+    }
+
+}
+
+
+int[] testArray = { 4, 2, 6, 5, 3, 9, 1, 10, 8, 7, 4 };
+
+int a = partition(testArray, 0, testArray.Length - 1);
+System.Console.WriteLine(a);
+System.Console.WriteLine(string.Join(", ", testArray));
+//System.Console.WriteLine(string.Join(", ", quickSort(testArray, 0, testArray.Length - 1)));
