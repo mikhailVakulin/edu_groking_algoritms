476ed24ce73f01f4eff57980ad5bfef41b035575
mikhail <mikhail.vakulin@gmail.com>
2022-09-12 22:24:36 +0300

    1

--- JS_course/index.js
+++ JS_course/index.js
@@ -1,4 +1,5 @@
 const objectA = {
+  //
   a: 10,
   b: true,
 };
@@ -10,6 +11,35 @@ copyOfA.c = "abc";
 
 console.log(objectA);
 
-const number = 1;
-number = 2;
-console.log(number);
+const myCity = {
+  city: "New York",
+  popular: true,
+  size: 10,
+  info: {
+    ispopular: true,
+  },
+};
+
+console.log(myCity.size);
+myCity.popular = false;
+myCity.countrie = "USA";
+
+console.log(myCity);
+
+delete myCity.popular; // удаление свойства объекта
+
+myCity["popular"] = true; // создание свойства через квадратные скобки
+myCity.countrie.ispopular = false;
+console.log(myCity);
+
+const name = "Mikhail";
+const postQty = 10;
+
+const userProfile = {
+  // сокращенный формат записи свойств
+  name, // работает если нжно присвоить свойствам
+  postQty, // значение обьявленных ранеее одноименных переменных
+  hasSignedAgreement: false,
+};
+
+console.log(userProfile);
