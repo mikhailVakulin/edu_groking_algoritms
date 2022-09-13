c24493459868ee6ede70597bdc580e4c5114b42c
mikhail <mikhail.vakulin@gmail.com>
2022-09-13 17:26:27 +0300

    1

---
+++ leetcode/longestContinuous/Program.cs
@@ -0,0 +1,30 @@
+﻿// Given an unsorted array of integers nums, return the length of the longest continuous increasing subsequence (i.e. subarray). The subsequence must be strictly increasing.
+
+// A continuous increasing subsequence is defined by two indices l and r (l < r) such that it is [nums[l], nums[l + 1], ..., nums[r - 1], nums[r]] and for each l <= i < r, nums[i] < nums[i + 1].
+
+
+
+// Example 1:
+
+// Input: nums = [1,3,5,4,7]
+// Output: 3
+// Explanation: The longest continuous increasing subsequence is [1,3,5] with length 3.
+// Even though [1,3,5,7] is an increasing subsequence, it is not continuous as elements 5 and 7 are separated by element
+// 4.
+
+int FindLengthOfLCIS(int[] nums)
+{
+    int counter = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] < nums[i + 1])
+        {
+            while (nums[i] < nums[i + 1])
+            {
+                counter++;
+                i++;
+            }
+        }
+    }
+}
+
---
+++ leetcode/shuflleTheArray/Program.cs
@@ -0,0 +1,36 @@
+﻿// Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+// Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+
+
+// Example 1:
+
+// Input: nums = [2,5,1,3,4,7], n = 3
+// Output: [2,3,5,4,1,7] 
+// Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
+
+
+int[] Shuffle(int[] nums, int n)
+{
+    int[] arrayX = new int[n];
+    int[] arrayY = new int[n];
+    int j = 0;
+    for (int i = 0; i < n; i++) arrayX[i] = nums[i];
+    for (int i = n; i < nums.Length; i++)
+    {
+        arrayY[j] = nums[i];
+        j++;
+    }
+    j = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+
+        nums[i] = arrayX[j];
+        i++;
+        nums[i] = arrayY[j];
+        j++;
+    }
+    return nums;
+}
+
