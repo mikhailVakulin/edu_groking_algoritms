84335c29d88ed0d06a46de0d1ba97e0f3e74754e
unknown <mikhailVakulin>
2022-10-27 22:37:21 +0300

    create folger tasks and solitions tasks

---
+++ JS_course/TASKS/06-data-types/finish.js
@@ -0,0 +1,31 @@
+/** ЗАДАЧА 6 - Типы данных
+ *
+ * 1. Объявите несколько переменных и присвойте им значения:
+ *  - строка
+ *  - число
+ *  - логическое
+ *  - null
+ *  - undefined
+ *  - объект
+ *  - массив
+ *
+ * 2. Выведите в консоль тип каждого из значений
+ * используя оператор typeof
+ */
+const itsString = "mikhail";
+const itsNumber = 1;
+const itsBoolean = true;
+const itsNull = null;
+const itsUndefined = undefined;
+const itsObject = {
+  name: "mikhail",
+  surname: "vakulin",
+};
+const itsArray = [1, 1, 1];
+console.log(typeof itsString);
+console.log(typeof itsNumber);
+console.log(typeof itsBoolean);
+console.log(typeof itsNull);
+console.log(typeof itsUndefined);
+console.log(typeof itsObject);
+console.log(typeof itsArray);
