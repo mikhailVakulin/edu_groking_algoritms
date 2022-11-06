2ced7aa36448d58ef9c9a69bf0c11fb734124d7a
unknown <mikhailVakulin>
2022-11-06 17:48:50 +0300

    solution tasks 51-52

--- JS_course/TASKS/51-rest-and-spread-operators/finish.js
+++ JS_course/TASKS/51-rest-and-spread-operators/finish.js
@@ -12,22 +12,25 @@
  * ПОДСКАЗКА: В этом задании вы должны использовать как оператор "rest",
  * так и оператор "spread".
  */
+function meanScore(...arr) {
+  if (arr.some((el) => typeof el !== "number"))
+    return "Все аргументы в вызове функции должны быть числами!";
 
-const scores1 = [0, 1.5, 2.5, 3.7]
-const scores2 = [1.7, 4.5, 0, 4.9, 5.0, 4.2]
-const scores3 = [1.3, 2.5, 1.9]
-const scores4 = ['abc', 1.3, true, 2.5, 1.9]
+  return arr.reduce((a, b) => a + b / arr.length).toFixed(2);
+}
+const scores1 = [0, 1.5, 2.5, 3.7];
+const scores2 = [1.7, 4.5, 0, 4.9, 5.0, 4.2];
+const scores3 = [1.3, 2.5, 1.9];
+const scores4 = ["abc", 1.3, true, 2.5, 1.9];
 
-console.log(meanScore(/* все элементы из массива "scores1" */))
+console.log(meanScore(...scores1));
 // 1.93
 
-console.log(meanScore(/* все элементы из массивов "scores1" и "scores2" */))
+console.log(meanScore(...scores1, ...scores2));
 // 2.8
 
-console.log(
-  meanScore(/* все элементы из массивов "scores1", "scores2" и "scores3" */)
-)
+console.log(meanScore(...scores1, ...scores2, ...scores3));
 // 2.59
 
-console.log(meanScore(/* все элементы из массива "scores4" */))
+console.log(meanScore(scores4));
 // Все аргументы в вызове функции должны быть числами!
--- JS_course/TASKS/52-default-function-parameters/finish.js
+++ JS_course/TASKS/52-default-function-parameters/finish.js
@@ -14,17 +14,26 @@
  * Внимательно сравните свои результаты с результатами тестовых вызовов
  */
 
-console.log(weatherForecast('Dubai', 'Солнечно'))
+// function weatherForecast(city, weather) {
+//   if (typeof weather !== "string") weather = "Отличная погода!";
+//   return `Прогноз погоды для города ${city}: ${weather}`;
+// }
+
+function weatherForecast(city, weather = "Отличная погода!") {
+  return `Прогноз погоды для города ${city}: ${weather}`;
+}
+
+console.log(weatherForecast("Dubai", "Солнечно"));
 // Прогноз погоды для города Dubai: Солнечно
 
-console.log(weatherForecast('London', 'Небольшой дождь'))
+console.log(weatherForecast("London", "Небольшой дождь"));
 // Прогноз погоды для города London: Небольшой дождь
 
-console.log(weatherForecast('Paris'))
+console.log(weatherForecast("Paris"));
 // Прогноз погоды для города Paris: Отличная погода!
 
-console.log(weatherForecast('Miami', ''))
+console.log(weatherForecast("Miami", ""));
 // Прогноз погоды для города Miami:
 
-console.log(weatherForecast('Las Vegas', undefined))
+console.log(weatherForecast("Las Vegas", undefined));
 // Прогноз погоды для города Las Vegas: Отличная погода!
