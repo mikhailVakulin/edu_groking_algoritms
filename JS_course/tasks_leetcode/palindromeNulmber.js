34f92fc0b6b2cf5deb1f6c0feed5a9ed879687dc
mikhail <mikhail.vakulin@gmail.com>
2022-12-08 00:59:49 +0300

    solution leetcode task

---
+++ JS_course/tasks_leetcode/palindromeNulmber.js
@@ -0,0 +1,32 @@
+// Example 1:
+
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+// Example 2:
+
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+// Example 3:
+
+// Input: x = 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+var isPalindrome = function (x) {
+  if (x < 0) return false;
+  const res = parseInt(
+    x
+      .toString()
+      .split("")
+      .reverse()
+      .reduce((sum, el) => sum + el)
+  );
+  if (x === res) return true;
+  if (x !== res) return false;
+};
+
+x = 12421;
+
+console.log(isPalindrome(x));
